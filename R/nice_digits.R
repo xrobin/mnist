823feb8a8b269ccc75d4dde38af480656749de00
Xavier Robin <xavalias-github@xavier.robin.name>
2014-06-18 14:04:06 +0200

    Fix R CMD check warnings

--- R/nice_digits.R
+++ R/nice_digits.R
@@ -1,6 +1,6 @@
 
-plot.mnist <- function(data = mnist$test$x, label = mnist$test$y + 1, model = prcomp(mnist$train$x),
-					   prediction = predict(model, data)[,1:2], reconstruction = tcrossprod(prediction, model$rotation[,1:2]),
+plot.mnist <- function(x = mnist$test$x, label = mnist$test$y + 1, model = prcomp(mnist$train$x),
+					   predictions = predict(model, x)[,1:2], reconstructions = tcrossprod(predictions, model$rotation[,1:2]),
 					   highlight.digits = c(11, 3, 2, 91, 20, 188, 92, 1, 111, 13),
 					   digits.col = c("#FF0000FF", "#0000FFFF", "#964B00FF", "#FF00FFFF", "#00AAAAFF", "#00EE00FF", "#000000FF", "#000000FF", "#AAAAAAFF", "#FF9900FF"),
 					   digits.alphas.reverse = c(FALSE, TRUE)[c(1, 1, 1, 1, 1, 1, 2, 1, 1, 1)],
@@ -14,7 +14,8 @@ plot.mnist <- function(data = mnist$test$x, label = mnist$test$y + 1, model = pr
 					   xlab = "Node 1",
 					   ylab = "Node 2",
 					   xlim = NULL,
-					   ylim = NULL
+					   ylim = NULL,
+					   ...
 					   ) {
 	
 	n.highlight <- length(highlight.digits)
@@ -25,13 +26,13 @@ plot.mnist <- function(data = mnist$test$x, label = mnist$test$y + 1, model = pr
 	alphas.gen <- expand.grid(c(0:9, LETTERS[1:6]), c(0:9, LETTERS[1:6]))
 	alphas <- paste(alphas.gen[,2], alphas.gen[,1], sep="")
 	
-	sapply(1:10, function(i) {show.digit(data[highlight.digits[i],], col=paste(str_sub(digits.col[label[highlight.digits[i]]], 1, 7), if (digits.alphas.reverse[i]) rev(alphas) else alphas, sep=""))})
-	sapply(1:10, function(i) {show.digit(reconstruction[highlight.digits[i],], col=paste(str_sub(digits.col[label[highlight.digits[i]]], 1, 7), if (digits.alphas.reverse[i]) rev(alphas) else alphas, sep=""))})
+	sapply(1:10, function(i) {show.digit(x[highlight.digits[i],], col=paste(substring(digits.col[label[highlight.digits[i]]], 1, 7), if (digits.alphas.reverse[i]) rev(alphas) else alphas, sep=""))})
+	sapply(1:10, function(i) {show.digit(reconstructions[highlight.digits[i],], col=paste(substring(digits.col[label[highlight.digits[i]]], 1, 7), if (digits.alphas.reverse[i]) rev(alphas) else alphas, sep=""))})
 	
 	par(cex=cex, mar=c(3, 3, 0, 0), mgp=c(2, 1, 0))
-	plot(prediction[, 1], prediction[, 2], xlab = xlab, ylab = ylab, xlim = xlim, ylim = ylim,
+	plot(predictions[, 1], predictions[, 2], xlab = xlab, ylab = ylab, xlim = xlim, ylim = ylim,
 		 pch = pch, bg = pch.bg[label], col=pch.col[label], cex.axis=cex.axis, cex.lab=cex.lab)
-	points(prediction[highlight.digits, 1], prediction[highlight.digits, 2], pch = pch, bg = pch.bg[label[highlight.digits]], col = pch.col[label[highlight.digits]], cex=cex.highlight)
+	points(predictions[highlight.digits, 1], predictions[highlight.digits, 2], pch = pch, bg = pch.bg[label[highlight.digits]], col = pch.col[label[highlight.digits]], cex=cex.highlight)
 	
 	par(opar)
 }
--- R/onAttach.R
+++ R/onAttach.R
@@ -3,11 +3,10 @@
 	# with library(mnist), so load it automatically
 	mnist_data_file <- system.file("data/mnist.RData", package="mnist")
 	if (mnist_data_file == "") {
-		cat("Downloading mnist dataset...")
+		packageStartupMessage("Downloading mnist dataset...")
 		mnist <- download.mnist()
 		dir.create(paste(system.file(package="mnist"), "data", sep=.Platform$file.sep))
 		save(mnist, file = paste(system.file(package="mnist"), "data", "mnist.RData", sep=.Platform$file.sep))
-		cat(" done!\n")
 	}
 	data(mnist) 
 	packageStartupMessage("'mnist' dataset loaded.")
